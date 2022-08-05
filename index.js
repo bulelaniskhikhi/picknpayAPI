f4d94927e98f2682d6b46e48944624e76399a6eb
bulelaniskhikhi <bulelaniskhikhi@gmail.com>
2022-08-05 11:45:48 +0200

    course error

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const port = parseInt(process.env.PORT) || 4000;
 app.use(router, cors(), express.json(), express.urlencoded({
     extended: true
 }));
-
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
 });
